f4b3e0a6a62f7a8b845de61630b61bbd2d01ce06
Edison Carrillo <djsat.ec@gmail.com>
2015-02-06 16:04:00 -0500

    add horario

---
+++ asys/app/models/Horario.java
@@ -0,0 +1,29 @@
+package models;
+
+public class Horario {
+
+	int Nro;
+	String docente;
+	String Hora;
+	String Lunes;
+	String Martes;
+	String Miercoles;
+	String Jueves;
+	String Viernes;
+	String Sabado;
+
+	public Horario(int Nro, String docente, String Hora, String Lunes,
+			String Martes, String Miercoles, String Jueves, String Viernes,
+			String Sabado) {
+		Nro = this.Nro;
+		docente = this.docente;
+		Hora = this.Hora;
+		Lunes = this.Lunes;
+		Martes = this.Martes;
+		Miercoles = this.Miercoles;
+		Jueves = this.Jueves;
+		Viernes = this.Viernes;
+		Sabado = this.Sabado;
+	}
+
+}
